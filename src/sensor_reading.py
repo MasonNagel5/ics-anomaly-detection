0e392799f4cc542fc7637bc686b9464dc75b1523
MasonNagel5 <masonanagel@gmail.com>
2026-06-25 16:14:16 -0700

    add flag_r6 to sensor reading

--- src/sensor_reading.py
+++ src/sensor_reading.py
@@ -7,10 +7,11 @@ class SensorReading:
         self.pressures = pressures  # dict
         self.att_flag = att_flag # BOOL flag 1 or 0
         
-        # Flags for each of the 5 rules, initialized to FALSE
-        self.flagged = False 
+        # Flags for each of the 6 rules, initialized to FALSE
+        self.flagged = False
         self.flag_r1 = False
         self.flag_r2 = False
         self.flag_r3 = False
         self.flag_r4 = False
         self.flag_r5 = False
+        self.flag_r6 = False
